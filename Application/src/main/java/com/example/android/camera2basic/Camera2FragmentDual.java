684c486ceda61edb2108c2f83f492d41a974812b
Salman-H <sah517@g.harvard.edu>
2019-01-23 20:37:50 -0500

    feat: Initialize SurfaceTextureListener for both cameras TextureView.SurfaceTextureListener handles lifecycle events on a TextureView.

--- Application/src/main/java/com/example/android/camera2basic/Camera2FragmentDual.java
+++ Application/src/main/java/com/example/android/camera2basic/Camera2FragmentDual.java
@@ -1,13 +1,49 @@
 package com.example.android.camera2basic;
 
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class Camera2FragmentDual extends Fragment {
+
+    // Hardcoded for now. Can be obtained from android.hardware.camera2.CameraCharacteristics
+    private static final String CAM_0_ID = "0";
+    private static final String CAM_1_ID = "1";
+
+    private CameraManager mCameraManager;
+
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener0 = initSurfaceTextureListener(CAM_0_ID);
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener1 = initSurfaceTextureListener(CAM_1_ID);
+
+    private TextureView.SurfaceTextureListener initSurfaceTextureListener(final String camId) {
+        return new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture,
+                                                  int width, int height) {
+                mCameraManager = (CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE);
+                openCamera(camId ,width, height);
+            }
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int width, int height) {
+                configureTransform(camId, width, height);
+            }
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+                return true; // not false
+            }
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+                // onSurfaceTextureUpdated()
+            }
+        };
+    }
 
     public static Camera2FragmentDual newInstance() {
         return new Camera2FragmentDual();
