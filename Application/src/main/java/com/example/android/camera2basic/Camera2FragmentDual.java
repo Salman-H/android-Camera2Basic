9b949737fc8d7edff31792eecafa6d73d85b2403
Salman-H <sah517@g.harvard.edu>
2019-01-23 19:06:35 -0500

    feat: Create new fragment for two cameras

---
+++ Application/src/main/java/com/example/android/camera2basic/Camera2FragmentDual.java
@@ -0,0 +1,26 @@
+package com.example.android.camera2basic;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Camera2FragmentDual extends Fragment {
+
+    public static Camera2FragmentDual newInstance() {
+        return new Camera2FragmentDual();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_camera2_dual, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, Bundle savedInstanceState) {
+    }
+}
